1eeed31786232e5403ff1be928a6e5b6870b5bd4
Muhamad Fadzil Ramli <fadzil@gravity-byte.com>
2018-09-21 16:43:39 +0800

    2nd commit

--- lib/acunetix_rest_api/connect.rb
+++ lib/acunetix_rest_api/connect.rb
@@ -56,5 +56,22 @@ module AcunetixRestApi
       return targets
     end
 
+    def get_target_last_scan_info(target_id)
+      # https://172.16.129.94:3443/api/v1/targets/25efe9d4-b9c5-40a7-961d-d4088f9fdc91
+      request = Net::HTTP::Get.new("/api/v1/targets/#{target_id}")
+      request["X-Auth"] = @api_key
+      request["Content-Type"] = "application/json"
+      response = @https.request(request)
+      json_hash = JSON.parse(response.body)
+    end
+
+    def get_scan_info(scan_id)
+      # https://172.16.129.94:3443/api/v1/targets/25efe9d4-b9c5-40a7-961d-d4088f9fdc91
+      request = Net::HTTP::Get.new("/api/v1/scans/#{scan_id}")
+      request["X-Auth"] = @api_key
+      request["Content-Type"] = "application/json"
+      response = @https.request(request)
+      json_hash = JSON.parse(response.body)
+    end
   end
 end
