24cf74dd7e072d7b41c671ad2b8960fab51ff97e
Muhamad Fadzil Ramli <fadzil@gravity-byte.com>
2018-10-15 20:10:31 +0800

    method uniq_findings return uniq_vulns & uniq_vuln_stats

--- lib/acunetix_rest_api/results.rb
+++ lib/acunetix_rest_api/results.rb
@@ -1,25 +1,25 @@
 module AcunetixRestApi
   class Results
     def self.uniq_findings(vulnerabilities)
-      vulns = vulnerabilities.group_by.map do |vulnerability|
+      uniq_vulns = vulnerabilities.group_by.map do |vulnerability|
         [vulnerability['vt_name'],
         vulnerability['severity']]
       end
 
-      uniq_vuln = Hash.new(0)
-      vulns.uniq.each do |vuln_name, severity|
+      uniq_vuln_stats = Hash.new(0)
+      uniq_vulns.uniq.each do |vuln_name, severity|
         case severity
         when 3
-          uniq_vuln[:high] += 1
+          uniq_vuln_stats[:high] += 1
         when 2
-          uniq_vuln[:medium] += 1
+          uniq_vuln_stats[:medium] += 1
         when 1
-          uniq_vuln[:low] += 1
+          uniq_vuln_stats[:low] += 1
         when 0
-          uniq_vuln[:info] += 1
+          uniq_vuln_stats[:info] += 1
         end
       end
-      return uniq_vuln
+      return uniq_vulns, uniq_vuln_stats
     end
   end
 end
